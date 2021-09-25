73e764db3c3ba6a3ada20e3db180158749a61831
ShikharSaini <shikharsaini25@gmail.com>
2021-09-25 11:40:27 +0530

    commit on Datepicker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import logo from './Grid.png';
-import DatePicker from "react-widgets/DatePicker";
-import Calendar from "react-widgets/Calendar";
-//import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-<Calendar />;
-<DatePicker placeholder="m/dd/yy" />;
+
+
 var flag=0;
 var ids="1";
 const e = React.createElement;
@@ -15,6 +14,8 @@ const cont=document.querySelector('#chatbody');
 const sending=document.getElementById('message');
 var userString="Shikhar"
 var currInput="";
+
+
 
 document.getElementById("messagesend").onclick = InputValues;
 
@@ -37,7 +38,9 @@ class UserMessage extends React.Component{
 class ThankYouIQ extends React.Component{
   render(){
     return e(
-      'div',{className:'chatting'},'Thank you for taking our IQ test '+'\n'+'Your Score is : '
+      'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
+      e('div',{className:'chatting'},'Thank you for taking our IQ test '+'\n'+'Your Score is : ')
     );
   }
 }
@@ -46,6 +49,7 @@ class ThirdQuestion extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'Q3. Water is to a pipe as ? is to a wire.'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThankYouIQ,'ThankYouIQ')}},'electricity'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThankYouIQ,'ThankYouIQ')}},'heat'),
@@ -59,6 +63,7 @@ class SecondQuestion extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'Q2. Which answer expresses the meaning opposite of that of the specified word best?'+'\n'+'Tough'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThirdQuestion,'SecondQuestion')}},'masculine'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThirdQuestion,'SecondQuestion')}},'tender'),
@@ -72,6 +77,7 @@ class StarterIQtest extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'Lets Begin Your IQ test '),
       e('div',{className:'chatting'},'Your First Question is Which number logically follows this series? : 4     6     9     6     14     6      ...'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,SecondQuestion,'SecondQuestion')}},'17'),
@@ -89,7 +95,9 @@ class StarterIQtest extends React.Component{
 class ThankYouSurvey extends React.Component{
   render(){
     return e(
-      'div',{className:'chatting'},'Your Answers have been recorded '+'\n'+'Thank You For Giving The Survey'
+      'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
+      e('div',{className:'chatting'},'Your Answers have been recorded '+'\n'+'Thank You For Giving The Survey')
     );
   }
 }
@@ -99,6 +107,7 @@ class Recommendation extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'How likely Are you to recommend this company?'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThankYouSurvey,'ThankYouSurvey')}},'Very likely'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,ThankYouSurvey,'ThankYouSurvey')}},'Somewhat likely'),
@@ -112,6 +121,7 @@ class DeliveryQuesion extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'Among these Features which ones do you like ?'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,Recommendation,'Recommendation')}},'Delivery Time'),
       e('button',{className:'butt',onClick:function() {AddComponent(++ids,Recommendation,'Recommendation')}},'Variety in Products'),
@@ -125,6 +135,7 @@ class StartSurvey extends React.Component{
   render(){
     return e(
       'div',{className:'Question'},
+      <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
       e('div',{className:'chatting'},'Hello Let us begin with your survey'),
       e('div',{className:'chatting'},'How Would You Rate Your Delivery Experience?'),
       e('div',{className:'butt-grp'},
@@ -167,14 +178,40 @@ class PanCard extends React.Component{
   }
 }
 
+var dae=new Date().toLocaleString();
+
+const Example =() => {
+  const [startDate, setStartDate] = useState(new Date());
+  useEffect(() => {
+    dae=startDate.toLocaleString();
+  });
+  return (
+    <DatePicker selected={startDate} onChange={(date) => {setStartDate(date);}}  />
+  );
+};
+
+class UserDate extends React.Component{
+  render(){
+    return e(
+      'div',{className:'Question'},
+      e('div',{className:'youclass'},'You'),
+      e('br'),
+      e('div',{className:'UserReplies', id:'dd'},dae),
+      e('br')
+    );
+  }
+}
+
 class DOB extends React.Component{
-  //const [startDate, setStartDate] = useState(new Date());
   render(){
     return e(
       'div',{className:'Question'},
       <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
-      e('div',{className:'chatting'},'Great going, now just enter your DOB'),
-        e('DatePicker',{defaultValue:new Date(),valueFormat:{ dateStyle: "medium" }})
+      e('div',{className:'chatting'},'Great, Now Please Select Your DOB'),
+      e('div',{id:'Date'},<Example/>),
+      e('button',{className:'butt',onClick:function() {AddComponent(++ids,UserDate,'UserDate');
+      sending.placeholder='Enter Pancard Number : for eg - 102083055';AddComponent(++ids,PanCard,'PanCard');}}
+      ,'Confirm Date'),
       );
   }
 }
@@ -184,7 +221,7 @@ class Number extends React.Component{
     return e(
       'div',{className:'Question'},
       <div id='image'><img src={logo} height={10}/><span>  GridBot</span></div>,
-      e('div',{id:'Question 3'},e('div',{className:'chatting'},'Thank You, Now please Enter Your Number '))
+      e('div',{className:'chatting'},'Thank You, Now please Enter Your Number ')
     );
   }
 }
@@ -263,7 +300,7 @@ function InputValues(){
       if(currInput=="Name")
       {sending.placeholder='Enter Number : for eg - 987*******';AddComponent(ids,Number,'Number');}
       else if(currInput=="Number")
-      {sending.placeholder='Enter DOB : for eg - 23/09/2001';AddComponent(ids,DOB,'DOB');}
+      {sending.placeholder='Enter DOB : for eg - DD/MM/YYYY';AddComponent(ids,DOB,'DOB');}
       else if(currInput=="DOB")
       {sending.placeholder='Enter Pancard Number : for eg - 102083055';AddComponent(ids,PanCard,'PanCard');}
       else if(currInput=="PanCard")
